b240c3ccdeb37620c97c2bbc5b1474b01b1216db
Aleksander Antoniewicz <a.m.antoniewicz@newcastle.ac.uk>
2014-12-14 20:00:16 +0100

    Transaction rows redesigned

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/AccountsFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/AccountsFragment.java
@@ -1,6 +1,7 @@
 package uk.ac.ncl.cs.team16.lloydsbankingapp;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -9,15 +10,19 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
-
+import java.util.List;
 
 
 public class AccountsFragment extends Fragment {
@@ -27,6 +32,8 @@ public class AccountsFragment extends Fragment {
     private String name = "John";
     private int balance = 120;
     private GregorianCalendar c;
+    private List<Transaction> transactionList;
+
 
     public AccountsFragment() {
         // Required empty public constructor
@@ -37,19 +44,49 @@ public class AccountsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        transactionList = new ArrayList<Transaction>();
+        addToList();
         setupMenu();
-        String[] items = {"fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
-                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
-                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
-                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja"};
+
 
         c = new GregorianCalendar();
 
         View v = inflater.inflate(R.layout.fragment_accounts, container, false);
         lv = (ListView) v.findViewById(R.id.transaction_listview);
-        lv.setAdapter(new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, items));
+        lv.setAdapter(new CustomAdapter());
         setupWelcomeScreen(v);
         return v;
+    }
+
+    private void addToList() {
+        transactionList.add(new Transaction("Homer Simpson Industry Co. Ltd.", "26/10/2014", "1245.10", "+20.00"));
+        transactionList.add(new Transaction("Alor Anfosdijgnhnjmpd", "26/11/2014", "20.00", "-20.00"));
+        transactionList.add(new Transaction("Szpam Szpamonov", "01/10/2014", "45.10", "-5.00"));
+        transactionList.add(new Transaction("I'm sending you money", "05/02/2014", "12.10", "-150.00"));
+        transactionList.add(new Transaction("Will", "09/10/2014", "120.00", "-200.29"));
+        transactionList.add(new Transaction("Michael", "12/10/2014", "10.00", "+250.29"));
+        transactionList.add(new Transaction("WM Morisson Supermarkets PLC", "20/01/2013", "-10.00", "+15.29"));
+    }
+
+    class CustomAdapter extends ArrayAdapter<Transaction>{
+        CustomAdapter(){
+            super(getActivity(), R.layout.transaction_row, R.id.payee_name, transactionList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = super.getView(position, convertView, parent);
+            TextView payee = (TextView) row.findViewById(R.id.payee_name);
+            payee.setText(transactionList.get(position).getPayee());
+            TextView date = (TextView) row.findViewById(R.id.transaction_date);
+            date.setText(transactionList.get(position).getDate());
+            TextView balance = (TextView) row.findViewById(R.id.after_balance);
+            balance.setText("£" + transactionList.get(position).getBalance());
+            TextView value = (TextView) row.findViewById(R.id.transaction_value);
+            value.setText(transactionList.get(position).getValue());
+            return row;
+        }
     }
 
     private void setupWelcomeScreen(View v) {
---
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Transaction.java
@@ -0,0 +1,34 @@
+package uk.ac.ncl.cs.team16.lloydsbankingapp;
+
+/**
+ * Created by Alek on 2014-12-14.
+ */
+public class Transaction{
+    private String payee;
+    private String date;
+    private String balance;
+    private String value;
+
+    public Transaction(String p, String d, String b, String v){
+        this.payee = p;
+        this.date = d;
+        this.balance = b;
+        this.value = v;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
