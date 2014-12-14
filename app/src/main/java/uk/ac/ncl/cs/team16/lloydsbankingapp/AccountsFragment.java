bc1c39390a4fc3b87ef39926c05b567a73acf6ab
Aleksander Antoniewicz <a.m.antoniewicz@newcastle.ac.uk>
2014-12-14 15:53:25 +0100

    Functional welcome Layout

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/AccountsFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/AccountsFragment.java
@@ -8,14 +8,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.GregorianCalendar;
 
 
 
 public class AccountsFragment extends Fragment {
 
+    private ListView lv;
     private OnFragmentInteractionListener mListener;
+    private String name = "John";
+    private int balance = 120;
+    private GregorianCalendar c;
 
     public AccountsFragment() {
         // Required empty public constructor
@@ -27,8 +38,30 @@ public class AccountsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         setupMenu();
+        String[] items = {"fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
+                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
+                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja",
+                "fsdfsd","hgfkghkgh","aghgfjkhgj","jgjghfsd","hdgjhfgja"};
 
-        return inflater.inflate(R.layout.fragment_accounts, container, false);
+        c = new GregorianCalendar();
+
+        View v = inflater.inflate(R.layout.fragment_accounts, container, false);
+        lv = (ListView) v.findViewById(R.id.transaction_listview);
+        lv.setAdapter(new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, items));
+        setupWelcomeScreen(v);
+        return v;
+    }
+
+    private void setupWelcomeScreen(View v) {
+        TextView welcomeTV, balanceTV, dateTv;
+        welcomeTV = (TextView) v.findViewById(R.id.welcome_tv);
+        balanceTV = (TextView) v.findViewById(R.id.balance_tv);
+        dateTv = (TextView) v.findViewById(R.id.date_tv);
+
+        welcomeTV.setText("Welcome, " + name);
+        balanceTV.setText("Current balance: £" + balance);
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        dateTv.setText(df.format(c.getTime()));
     }
 
     // TODO: Rename method, update argument and hook method into UI event
