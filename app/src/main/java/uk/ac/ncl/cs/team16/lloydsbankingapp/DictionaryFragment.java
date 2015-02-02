9999eeb87a7fef62185a256179dae73181617b17
James Mountain <james.w.mountain@gmail.com>
2015-02-02 19:22:30 +0000

    Implemented live search in dictionary. Added auto sorting to dictionary (this may not be necessary later if the dictionary is already sorted in the DB)

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/DictionaryFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/DictionaryFragment.java
@@ -4,17 +4,21 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 public class DictionaryFragment extends Fragment {
 
@@ -26,29 +30,75 @@ public class DictionaryFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View dictionaryView = inflater.inflate(R.layout.fragment_dictionary, container, false);
-        dictionaryEntries = new ArrayList<DictionaryEntry>();
-        dictionaryEntries.add(new DictionaryEntry("Interest", "Interest is a payment by the bank for your deposit."));
-        dictionaryEntries.add(new DictionaryEntry("Overdraft", "An extra amount of available money below £0. This is not unlimited."));
-        dictionaryEntries.add(new DictionaryEntry("Standing Order", "A set payment (usually with a timescale), which is setup by you."));
-
-        List<Map<String, String>> dictionaryMapList = new ArrayList<Map<String, String>>(); // I'll clean this up later, it's a mess
+    private List<Map<String,String>> rebuildDictionary(String searchPattern) {
+        List<Map<String, String>> dictionaryMapList = new ArrayList<Map<String, String>>();
         for (DictionaryEntry entry : dictionaryEntries) {
             Map<String, String> dictionaryMap = new HashMap<String, String>();
+            String entryName = entry.getEntryName();
+            int searchLength = Math.min(searchPattern.length(), entryName.length());
+            if (!searchPattern.equals("")) {
+                if (!searchPattern.toLowerCase().equals(entryName.toLowerCase().substring(0, searchLength))) {
+                    continue;
+                }
+            }
+
             dictionaryMap.put("name", entry.getEntryName());
             dictionaryMap.put("desc", entry.getEntryDescription());
             dictionaryMapList.add(dictionaryMap);
         }
 
+        Collections.sort(dictionaryMapList, new Comparator<Map<String, String>>() {
+            @Override
+            public int compare(Map<String, String> map1, Map<String, String> map2) {
+                return map1.get("name").compareTo(map2.get("name"));
+            }
+        });
+
+        return dictionaryMapList;
+    }
+
+    private void populateDictionary() {
+        dictionaryEntries.add(new DictionaryEntry("Interest", "Interest is a payment by the bank for your deposit."));
+        dictionaryEntries.add(new DictionaryEntry("Overdraft", "An extra amount of available money below £0. This is not unlimited."));
+        dictionaryEntries.add(new DictionaryEntry("Standing Order", "A set payment (usually with a timescale), which is setup by you."));
+        dictionaryEntries.add(new DictionaryEntry("Student Loan", "A sum of money that is paid to the bank holder by the government in order to pay for student expenses."));
+        dictionaryEntries.add(new DictionaryEntry("Statement", "A set of transactions put into a summary over a time period, normally one month."));
+        dictionaryEntries.add(new DictionaryEntry("ATM", "A machine to provide an interface to get banking services, via ones card."));
+        dictionaryEntries.add(new DictionaryEntry("Available Balance", "Calculated value which will take into account not only your balance but your overdraft."));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View dictionaryView = inflater.inflate(R.layout.fragment_dictionary, container, false);
+        dictionaryEntries = new ArrayList<DictionaryEntry>();
+        populateDictionary();
+
         dictionarySearchResults = (ListView) dictionaryView.findViewById(R.id.dictionarySearchRes);// Set it to the entire dictionary list, initially
-        String[] resource = {"name", "desc"};
-        int[] location = {android.R.id.text1, android.R.id.text2};
-        SimpleAdapter adapter = new SimpleAdapter(getActivity(), dictionaryMapList, android.R.layout.simple_list_item_2, resource, location);
+        final String[] resource = {"name", "desc"};
+        final int[] location = {android.R.id.text1, android.R.id.text2};
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), rebuildDictionary(""), android.R.layout.simple_list_item_2, resource, location);
         dictionarySearchResults.setAdapter(adapter);
+
+        final EditText searchBar = (EditText) dictionaryView.findViewById(R.id.dictionarySearchBar);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence cs, int i, int i2, int i3) {
+                // REQUIRED
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cs, int i, int i2, int i3) {
+                SimpleAdapter searchAdapter = new SimpleAdapter(getActivity(), rebuildDictionary(searchBar.getText().toString()), android.R.layout.simple_list_item_2, resource, location);
+                dictionarySearchResults.setAdapter(searchAdapter);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //ALSO REQUIRED
+            }
+        });
 
         return dictionaryView;
     }
