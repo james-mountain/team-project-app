11b515d66985d9540c5cd611e228e2495b620e54
James Mountain <james.w.mountain@gmail.com>
2015-02-14 17:05:14 +0000

    Review fragment started, decided to go with the spinner method for simplicity

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/ReviewFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/ReviewFragment.java
@@ -13,12 +13,22 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 
 public class ReviewFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private List<Payment> payments;
 
     public ReviewFragment() {
         // Required empty public constructor
@@ -29,7 +39,14 @@ public class ReviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false);
+        View reviewView = inflater.inflate(R.layout.fragment_review, container, false); // This will work by using a spinner to inflate different fragments
+        ListView paymentsListView = (ListView) reviewView.findViewById(R.id.reviewList);
+
+        payments = new ArrayList<Payment>();
+        payments.add(new Payment(342, "Rob Robertson", new GregorianCalendar(2010, 05, 10), "33.00"));
+        payments.add(new Payment(20, "Thomas Tommyson", new GregorianCalendar(2013, 03, 16), "11.57"));
+        paymentsListView.setAdapter(new PaymentAdapter());
+        return reviewView;
     }
 
     @Override
@@ -55,4 +72,25 @@ public class ReviewFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    private class PaymentAdapter extends ArrayAdapter<Payment> {
+        PaymentAdapter(){
+            super(getActivity(), R.layout.payment_row, R.id.paymentNumberText, payments);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View paymentRow = super.getView(position, convertView, parent);
+            TextView paymentNumberTextView = (TextView) paymentRow.findViewById(R.id.paymentNumberText);
+            TextView payeeTextView = (TextView) paymentRow.findViewById(R.id.payeeText);
+            TextView payeeDateTextView = (TextView) paymentRow.findViewById(R.id.payeeDateText);
+            DateFormat payeeDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            TextView paymentAmountText = (TextView) paymentRow.findViewById(R.id.paymentAmountText);
+
+            paymentNumberTextView.setText("" + payments.get(position).getPaymentNumber());
+            payeeTextView.setText(payments.get(position).getPayee());
+            payeeDateTextView.setText(payeeDateFormat.format(payments.get(position).getDate().getTime()));
+            paymentAmountText.setText("£" + payments.get(position).getAmount());
+            return paymentRow;
+        }
+    }
 }
