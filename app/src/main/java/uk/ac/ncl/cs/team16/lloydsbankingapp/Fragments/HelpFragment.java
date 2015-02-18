2087ae3f3a21231c415d152516fd98009f16dfed
Shawkat-Roy <shawkat_ck@hotmail.co.uk>
2015-02-18 18:02:05 +0000

    Help fragment expandable list view now works. just needs visual improvements and will be complete (i think)

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/HelpFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/HelpFragment.java
@@ -14,7 +14,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import uk.ac.ncl.cs.team16.lloydsbankingapp.Models.ExpandableListAdapter;
 import uk.ac.ncl.cs.team16.lloydsbankingapp.R;
 
 
@@ -27,8 +32,11 @@ import uk.ac.ncl.cs.team16.lloydsbankingapp.R;
 public class HelpFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
-    private String[] helpOptions = {"How to reset online login credentials", "How to log out of you account",
-            "Leave feedback"};
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView listView;
+    List<String> listHeaders;
+    HashMap<String, List<String>> listItems;
 
     public HelpFragment() {
         // Required empty public constructor
@@ -38,10 +46,43 @@ public class HelpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+
+        View helpView = inflater.inflate(R.layout.fragment_help, container, false);
+
+        listView = (ExpandableListView) helpView.findViewById(R.id.helpListView);
+        prepareListData();
+        listAdapter = new ExpandableListAdapter(getActivity(), listItems, listHeaders);
+        listView.setAdapter(listAdapter);
+
+        return helpView;
     }
 
+    private void prepareListData() {
+
+        listHeaders = new ArrayList<String>();
+        listItems = new HashMap<String, List<String>>();
+
+        listHeaders.add("How to reset online login credentials");
+        listHeaders.add("How to log out");
+        listHeaders.add("Support");
+
+        List<String> reset = new ArrayList<String>();
+        reset.add("For security purposes, you cannot reset your login credentials via this app. " +
+                "If you wish to do this, you will need to speak with your local branch in person. " +
+                "Please note that you may be asked to show ID in store");
+
+        List<String> logOut = new ArrayList<String>();
+        logOut.add("To log out, simply use the back button on your device to take the app back " +
+                "to the login screen");
+
+        List<String> support = new ArrayList<String>();
+        support.add("If you have encountered any technical faults with this application or need " +
+                "help of any kind, contact the support team on 0800 000 0000");
+
+        listItems.put(listHeaders.get(0), reset);
+        listItems.put(listHeaders.get(1), logOut);
+        listItems.put(listHeaders.get(2), support);
+    }
 
     @Override
     public void onAttach(Activity activity) {
---
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Models/ExpandableListAdapter.java
@@ -0,0 +1,106 @@
+package uk.ac.ncl.cs.team16.lloydsbankingapp.Models;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.content.Context;
+import java.util.List;
+import java.util.HashMap;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import uk.ac.ncl.cs.team16.lloydsbankingapp.R;
+import android.graphics.Typeface;
+
+/**
+ * Created by Roy on 18/02/2015.
+ */
+public class ExpandableListAdapter extends BaseExpandableListAdapter{
+
+    private Context context;
+    private HashMap<String, List<String>> listDataItems;
+    private List<String> listDataHeaders;
+
+    public ExpandableListAdapter(Context context, HashMap<String, List<String>> listDataItems, List<String> listDataHeaders) {
+
+        this.context = context;
+        this.listDataItems = listDataItems;
+        this.listDataHeaders = listDataHeaders;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.listDataHeaders.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listDataItems.get(this.listDataHeaders.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listDataHeaders.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+
+        return this.listDataItems.get(this.listDataHeaders.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.help_list_header, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.helpListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.help_list_item, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.helpListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
