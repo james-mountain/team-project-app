7ab393a8699f806982a8c0b0b7aeecc61b507043
James Mountain <james.w.mountain@gmail.com>
2015-02-18 14:58:20 +0000

    Implemented the other two tabs of ReviewFragment. Added functionality for date prefixes (where appropriate) Fixed issue with large numbers and wrapping text

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/ReviewFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/ReviewFragment.java
@@ -31,29 +31,51 @@ import uk.ac.ncl.cs.team16.lloydsbankingapp.R;
 public class ReviewFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
-    private List<Payment> payments;
+    private List<Payment> payeePayments;
+    private List<Payment> standingPayments;
+    private List<Payment> debitPayments;
 
     public ReviewFragment() {
         // Required empty public constructor
     }
 
+    private void populatePaymentList() {
+        payeePayments = new ArrayList<Payment>();
+        payeePayments.add(new Payment(20, "Thomas Tommyson", new GregorianCalendar(2010, 03, 16), "11.57"));
+        payeePayments.add(new Payment(34, "Rob Robertson", new GregorianCalendar(2013, 05, 10), "33.00"));
+        payeePayments.add(new Payment(60, "Simon Simmonson", new GregorianCalendar(2014, 11, 03), "21.93"));
+
+        standingPayments = new ArrayList<Payment>();
+        standingPayments.add(new Payment(21, "Mr asdasda", new GregorianCalendar(2014, 01, 10), "3.90"));
+        standingPayments.add(new Payment(99, "Company 123", new GregorianCalendar(2015, 02, 12), "51.00"));
+        standingPayments.add(new Payment(34, "Mr fgsdhs", new GregorianCalendar(2011, 10, 03), "105.50"));
+
+        debitPayments = new ArrayList<Payment>();
+        debitPayments.add(new Payment(121, "Electric Bill Company", new GregorianCalendar(2006, 07, 13), "99.31"));
+        debitPayments.add(new Payment(131, "Student Finance 9912", new GregorianCalendar(2014, 11, 25), "410.00"));
+        debitPayments.add(new Payment(159, "Newcastle University", new GregorianCalendar(2013, 06, 22), "899.99"));
+    }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View reviewView = inflater.inflate(R.layout.fragment_review, container, false); // This will work by using a spinner to inflate different fragments
-        ListView paymentsListView = (ListView) reviewView.findViewById(R.id.payeeListView);
+        View reviewView = inflater.inflate(R.layout.fragment_review, container, false);
+        ListView paymentsListView = (ListView) reviewView.findViewById(R.id.payeeListView); // I could implement these as a list, but is it worth it?
+        ListView standingListView = (ListView) reviewView.findViewById(R.id.standingListView);
+        ListView debitListView = (ListView) reviewView.findViewById(R.id.debitListView);
+
         TabHost tabHost = (TabHost) reviewView.findViewById(R.id.tabHost);
         tabHost.setup();
         tabHost.addTab(tabHost.newTabSpec("payees").setIndicator("Payees").setContent(R.id.payeeTab));
         tabHost.addTab(tabHost.newTabSpec("standingorders").setIndicator("Standing Orders").setContent(R.id.standingTab));
         tabHost.addTab(tabHost.newTabSpec("directdebits").setIndicator("Direct Debits").setContent(R.id.debitTab));
 
-        payments = new ArrayList<Payment>();
-        payments.add(new Payment(342, "Rob Robertson", new GregorianCalendar(2010, 05, 10), "33.00"));
-        payments.add(new Payment(20, "Thomas Tommyson", new GregorianCalendar(2013, 03, 16), "11.57"));
-        paymentsListView.setAdapter(new PaymentAdapter());
+        populatePaymentList();
+
+        paymentsListView.setAdapter(new PaymentAdapter(payeePayments, "Last: "));
+        standingListView.setAdapter(new PaymentAdapter(standingPayments, "Next: "));
+        debitListView.setAdapter(new PaymentAdapter(debitPayments, "Last: "));
 
         tabHost.setCurrentTab(0);
         return reviewView;
@@ -83,8 +105,13 @@ public class ReviewFragment extends Fragment {
     }
 
     private class PaymentAdapter extends ArrayAdapter<Payment> {
-        PaymentAdapter(){
-            super(getActivity(), R.layout.payment_row, R.id.paymentNumberText, payments);
+        private List<Payment> paymentArray;
+        private String datePrefix = "";
+
+        PaymentAdapter(List<Payment> paymentArray, String datePrefix){
+            super(getActivity(), R.layout.payment_row, R.id.paymentNumberText, paymentArray);
+            this.paymentArray = paymentArray;
+            this.datePrefix = datePrefix;
         }
 
         @Override
@@ -96,10 +123,10 @@ public class ReviewFragment extends Fragment {
             DateFormat payeeDateFormat = new SimpleDateFormat("dd/MM/yyyy");
             TextView paymentAmountText = (TextView) paymentRow.findViewById(R.id.paymentAmountText);
 
-            paymentNumberTextView.setText("" + payments.get(position).getPaymentNumber());
-            payeeTextView.setText(payments.get(position).getPayee());
-            payeeDateTextView.setText(payeeDateFormat.format(payments.get(position).getDate().getTime()));
-            paymentAmountText.setText("£" + payments.get(position).getAmount());
+            paymentNumberTextView.setText("" + paymentArray.get(position).getPaymentNumber());
+            payeeTextView.setText(paymentArray.get(position).getPayee());
+            payeeDateTextView.setText(datePrefix + payeeDateFormat.format(paymentArray.get(position).getDate().getTime()));
+            paymentAmountText.setText("£" + paymentArray.get(position).getAmount());
             return paymentRow;
         }
     }
