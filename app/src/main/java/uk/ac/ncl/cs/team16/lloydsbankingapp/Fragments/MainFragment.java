2c831130c6a97adfc0ae6c6d741c3fdd5d9e77a1
James Mountain <james.w.mountain@gmail.com>
2015-03-08 01:49:29 +0000

    You can now obtain a fully authenticated session ID from the login button. I still need to figure out a way to pass the session ID to the HomeActivity class. From there it's easy to make calls to backend (minor modifications needed) I used a Apache Library to do a SHA256 HEX on the password and memorable word, if i i didn't, it would be very difficult. You can't "login" right now, only get success fail messages back. I'll fix this tommorow.

--- app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/MainFragment.java
+++ app/src/main/java/uk/ac/ncl/cs/team16/lloydsbankingapp/Fragments/MainFragment.java
@@ -14,19 +14,145 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import uk.ac.ncl.cs.team16.lloydsbankingapp.Activities.HomeActivity;
 import uk.ac.ncl.cs.team16.lloydsbankingapp.R;
-
+import uk.ac.ncl.cs.team16.lloydsbankingapp.network.VolleySingleton;
 
 public class MainFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     private Button loginButton;
+    private static final String AUTH_URL_BASE = "http://csc2022api.sitedev9.co.uk/authentication";
+    private static final String APPKEY = "FZpQ25vZsV8X1btAn59VgA2Bfe37kXMP";
+    private static String sessionToken = null;
+
     public MainFragment() {
         // Required empty public constructor
     }
 
+    private void getSessionToken() {
+        RequestQueue networkQueue = VolleySingleton.getInstance().getRequestQueue();
+        Map<String, String> appKeyParams = new HashMap<String, String>();
+        appKeyParams.put("app_key", APPKEY);
+
+        JsonObjectRequest sessTokenRequest = new JsonObjectRequest(Request.Method.POST, AUTH_URL_BASE + "/session", new JSONObject(appKeyParams), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    sessionToken = response.getString("session_id");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                requestLogin();
+            }
+        },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // oops, error handling can be sorted later.
+            }
+        });
+        networkQueue.add(sessTokenRequest);
+    }
+
+    private void requestLogin() {
+        String userID = "1";
+        String password = "password123";
+
+        RequestQueue networkQueue = VolleySingleton.getInstance().getRequestQueue();
+        Map<String, String> loginParams = new HashMap<String, String>();
+        String passwordHex = new String(Hex.encodeHex(DigestUtils.sha256(password))); // Apache Common Codec - DigestUtils lib
+        String combinedHex = new String(Hex.encodeHex(DigestUtils.sha256(sessionToken + passwordHex)));
+
+        loginParams.put("userid", userID);
+        loginParams.put("password", combinedHex);
+
+        JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, AUTH_URL_BASE + "/login", new JSONObject(loginParams), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String status = response.getString("status");
+                    if (status.equals("Success")) {
+                        requestAuth();
+                    } else {
+                        Toast.makeText(getActivity(), "Incorrect username or password.", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // oops, error handling can be sorted later.
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("API-SESSION-ID", sessionToken);
+                return headers;
+            }
+        };
+        networkQueue.add(loginRequest);
+    }
+
+    private void requestAuth() {
+        String memorable = "password123";
+
+        RequestQueue networkQueue = VolleySingleton.getInstance().getRequestQueue();
+        Map<String, String> loginParams = new HashMap<String, String>();
+        String memHex = new String(Hex.encodeHex(DigestUtils.sha256(memorable))); // Apache Common Codec - DigestUtils lib
+        String combinedHex = new String(Hex.encodeHex(DigestUtils.sha256(sessionToken + memHex)));
+
+        loginParams.put("memorable", combinedHex);
+
+        JsonObjectRequest authRequest = new JsonObjectRequest(Request.Method.POST, AUTH_URL_BASE + "/authorize", new JSONObject(loginParams), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String status = response.getString("status");
+                    if (status.equals("Success")) {
+                        Toast.makeText(getActivity(), "SUCCESSFULLY LOGGED IN", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Incorrect memorable word.", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // oops, error handling can be sorted later.
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("API-SESSION-ID", sessionToken);
+                return headers;
+            }
+        };
+        networkQueue.add(authRequest);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,8 +170,9 @@ public class MainFragment extends Fragment {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent a = new Intent(getActivity(), HomeActivity.class);
-                startActivity(a);
+                getSessionToken();
+                /*Intent a = new Intent(getActivity(), HomeActivity.class);
+                startActivity(a);*/
             }
         });
     }
